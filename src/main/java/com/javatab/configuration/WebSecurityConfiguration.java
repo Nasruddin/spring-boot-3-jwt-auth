4d14f60d67379aec1e58156fa5540548205ca46b
Nasruddin <nasruddin.java@gmail.com>
2022-08-04 14:48:57 +0530

    Removed deprecated WebSecurityConfigurerAdapter dependency

--- src/main/java/com/javatab/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/javatab/configuration/WebSecurityConfiguration.java
@@ -3,98 +3,89 @@ package com.javatab.configuration;
 import com.javatab.security.AuthenticationTokenFilter;
 import com.javatab.security.EntryPointUnauthorizedHandler;
 import com.javatab.security.TokenUtils;
-import com.javatab.service.SecurityService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter{
+@RequiredArgsConstructor
+public class WebSecurityConfiguration {
 
-  private final EntryPointUnauthorizedHandler unauthorizedHandler;
-  private final UserDetailsService userDetailsService;
-  private final SecurityService securityService;
-  private final TokenUtils tokenUtils;
-  private final AuthenticationConfiguration configuration;
+    private final EntryPointUnauthorizedHandler unauthorizedHandler;
+    private final UserDetailsService userDetailsService;
+    private final TokenUtils tokenUtils;
+    private final AuthenticationConfiguration authConfig;
 
-  public WebSecurityConfiguration(EntryPointUnauthorizedHandler unauthorizedHandler, UserDetailsService userDetailsService, SecurityService securityService, TokenUtils tokenUtils, AuthenticationConfiguration configuration) {
-    this.unauthorizedHandler = unauthorizedHandler;
-    this.userDetailsService = userDetailsService;
-    this.securityService = securityService;
-    this.tokenUtils = tokenUtils;
-    this.configuration = configuration;
-  }
 
-  @Override
-  public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-    authenticationManagerBuilder
-      .userDetailsService(this.userDetailsService)
-      .passwordEncoder(this.passwordEncoder());
-  }
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder(10);
-  }
+    @Bean
+    public AuthenticationManager authenticationManager() throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
 
-  @Bean
-  public AuthenticationManager authenticationManage() throws Exception {
-    return configuration.getAuthenticationManager();
-  }
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(this.userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
 
-  @Bean
-  public AuthenticationTokenFilter authenticationTokenFilterBean() throws Exception {
-    AuthenticationTokenFilter authenticationTokenFilter = new AuthenticationTokenFilter(tokenUtils, userDetailsService);
-    authenticationTokenFilter.setAuthenticationManager(authenticationManage());
-    return authenticationTokenFilter;
-  }
+    @Bean
+    public AuthenticationTokenFilter authenticationTokenFilterBean() throws Exception {
+        AuthenticationTokenFilter authenticationTokenFilter = new AuthenticationTokenFilter(tokenUtils, userDetailsService);
+        authenticationTokenFilter.setAuthenticationManager(authenticationManager());
+        return authenticationTokenFilter;
+    }
 
-  @Bean
-  public SecurityService securityService() {
-    return this.securityService;
-  }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(this.unauthorizedHandler)
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                .antMatchers("/auth/**",
+                        "/h2-console/**",
+                        "/api-docs/**",
+                        "/swagger-ui.html",
+                        "/swagger-ui/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .cors();
 
-  protected void configure(HttpSecurity httpSecurity) throws Exception {
-    httpSecurity
-      .csrf()
-        .disable()
-      .exceptionHandling()
-        .authenticationEntryPoint(this.unauthorizedHandler)
-        .and()
-      .sessionManagement()
-        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        .and()
-      .authorizeRequests()
-        .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-        .antMatchers("/auth/**",
-                "/h2-console/**",
-                "/api-docs/**",
-                "/swagger-ui.html",
-                "/swagger-ui/**").permitAll()
-        .anyRequest().authenticated()
-         .and()
-      .cors();
+        // To fix h2-console - https://stackoverflow.com/questions/53395200/h2-console-is-not-showing-in-browser
+        http.headers().frameOptions().disable();
 
-    // To fix h2-console - https://stackoverflow.com/questions/53395200/h2-console-is-not-showing-in-browser
-    httpSecurity.headers().frameOptions().disable();
+        // Custom JWT based authentication
+        http
+                .addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
 
-    // Custom JWT based authentication
-    httpSecurity
-      .addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class);
-  }
-
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/images/**", "/js/**", "/webjars/**");
+    }
 }
