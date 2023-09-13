d20b3b104b06cb14f0055ee3172e2233af8beeee
Nasruddin <nasruddin.java@gmail.com>
2023-09-13 12:19:51 +0530

    Remove unwanted lombok annotation

--- src/main/java/com/javatab/exception/validators/Violation.java
+++ src/main/java/com/javatab/exception/validators/Violation.java
@@ -1,11 +1,5 @@
 package com.javatab.exception.validators;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@RequiredArgsConstructor
 public record Violation(String fieldName, String message) {
 }
